9a9a33b029851c45543af57c548657af8730fd5f
toughie-22 <pelumiadeniyi97@gmail.com>
2026-04-03 17:43:32 +0100

    Update frontend to connect to live Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://response-tracker-application-for-a.onrender.com/api'
 });
 
 // Configure Axios to automatically attach the token to every request
